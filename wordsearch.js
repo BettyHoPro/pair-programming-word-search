3d82d5271ff23b8bc91fcdf031e3142f40e636dc
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-10 17:18:35 -0800

    PAir programming finish Feb

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  } else {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+    const modifiedArr = transpose(letters);
+    const modifiedArrMap = modifiedArr.map((ls) => ls.join(""));
+    if (horizontalJoin.includes(word) || modifiedArrMap.includes(word)) {
+      return true;
+    } else {
+      return false;
     }
-}
+  }
+};
 
-module.exports = wordSearch
+const transpose = function (matrix) {
+  let array = [];
+  for (let row = 0; row < matrix[0].length; row++) {
+    let rowArray = [];
+    for (let column = 0; column < matrix.length; column++) {
+      rowArray.push(matrix[column][row]);
+    }
+    array.push(rowArray);
+  }
+  return array;
+};
+
+module.exports = wordSearch;
